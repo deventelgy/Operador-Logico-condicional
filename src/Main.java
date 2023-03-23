39bd8d1b83b54a59e141c1f30ccafa4d56702bbe
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-23 15:34:42 -0500

    Uso de la operación lógica condicional:  &&

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int students =150;
+        int rooms=0;
+
+        if(rooms!=0 && students/rooms>30){ // primero decidimos que rooms sea diferente que cero para que evalúe la siguiente condición
+            System.out.println("Crowded");
+        }
+    }
+}
